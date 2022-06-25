d2ddb6f56391e5a6d437c40f747a456dd9d14120
Ahmad Fahrudin <ahmadrockets@gmail.com>
2022-06-25 23:42:12 +0700

    first commit

---
+++ resources/views/layouts_stisla/main.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>{{ config('app.name') }}</title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{asset('stisla/modules/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('stisla/modules/fontawesome/css/all.min.css')}}">
+
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href="{{asset('stisla/modules/jqvmap/dist/jqvmap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('stisla/modules/weather-icon/css/weather-icons.min.css')}}">
+  <link rel="stylesheet" href="{{asset('stisla/modules/weather-icon/css/weather-icons-wind.min.css')}}">
+  <link rel="stylesheet" href="{{asset('stisla/modules/summernote/summernote-bs4.css')}}">
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{asset('stisla/css/style.css')}}">
+  <link rel="stylesheet" href="{{asset('stisla/css/components.css')}}">
+  @yield('css')
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
+
+<body>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+      @include('layouts_stisla.navbar')
+      @include('layouts_stisla.sidebar')
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          @yield('content')
+        </section>
+      </div>
+      @include('layouts_stisla.footer')
+    </div>
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="{{asset('stisla/modules/jquery.min.js')}}"></script>
+  <script src="{{asset('stisla/modules/popper.js')}}"></script>
+  <script src="{{asset('stisla/modules/tooltip.js')}}"></script>
+  <script src="{{asset('stisla/modules/bootstrap/js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('stisla/modules/nicescroll/jquery.nicescroll.min.js')}}"></script>
+  <script src="{{asset('stisla/modules/moment.min.js')}}"></script>
+  <script src="{{asset('stisla/js/stisla.js')}}"></script>
+  
+  <!-- JS Libraies -->
+  <script src="{{asset('stisla/modules/simple-weather/jquery.simpleWeather.min.js')}}"></script>
+  <script src="{{asset('stisla/modules/chart.min.js')}}"></script>
+  <script src="{{asset('stisla/modules/jqvmap/dist/jquery.vmap.min.js')}}"></script>
+  <script src="{{asset('stisla/modules/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
+  <script src="{{asset('stisla/modules/summernote/summernote-bs4.js')}}"></script>
+  <script src="{{asset('stisla/modules/chocolat/dist/js/jquery.chocolat.min.js')}}"></script>
+
+  <!-- Page Specific JS File -->
+  <script src="{{asset('stisla/js/page/index-0.js')}}"></script>
+  
+  <!-- Template JS File -->
+  <script src="{{asset('stisla/js/scripts.js')}}"></script>
+  <script src="{{asset('stisla/js/custom.js')}}"></script>
+
+  @stack('scripts')
+</body>
+</html>
